b247b00ae7db6fea60869617c01ac31ddc186b70
beugenius <brycehutchinson@mail.weber.edu>
2022-06-26 18:27:40 -0600

    commenting out because file is not needed

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-document.querySelector('#startButton').addEventListener('click', makeReq)
+// document.querySelector('#startButton').addEventListener('click', makeReq)
 
-async function makeReq() {
-    const symbolNum = parseInt(document.getElementById('numSymbols').value);
-    if(symbolNum > 8) {
-        symbolNum = 8;
-    }
-    const res = await fetch(`/api?symbolNums=${symbolNum}`);
-    const data = await res.json();
-}
+// async function makeReq() {
+//     const symbolNum = parseInt(document.getElementById('numSymbols').value);
+//     if(symbolNum > 8) {
+//         symbolNum = 8;
+//     }
+//     const res = await fetch(`/api?symbolNums=${symbolNum}`);
+//     const data = await res.json();
+// }
 
